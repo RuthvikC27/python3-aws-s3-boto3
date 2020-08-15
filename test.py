afe85557f2cc7133b635fbf49a300fd02b9f6b51
Ruthvik Vanam <ruthvikvanam27@gmail.com>
2020-08-15 16:48:14 +0530

    initial

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ AWS_ACCESS_KEY_ID = ""
 AWS_SECRET_ACCESS_KEY = ""
 AWS_STORAGE_BUCKET_NAME = ""
 
+
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
@@ -23,6 +24,17 @@ s3 = boto3.resource(
 # for bucket in s3.buckets.all():
 #     print(bucket.name)
 
-s3.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key='images/test.jpg', Body=data)
+# Upload file
+file_upload = s3.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key='images/test1.jpg', Body=data)
+url = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{"images/test1.jpg"}'
+# print(file_upload)
+print(url)
 
-# print("Done")
+# Download
+s3.Bucket(AWS_STORAGE_BUCKET_NAME).download_file('images/test1.jpg', './bitmoji.png')
+
+# Read text
+obj = s3.Object(AWS_STORAGE_BUCKET_NAME, 'images/test1.jpg')
+body = obj.get()['Body'].read()
+print(body)
+
